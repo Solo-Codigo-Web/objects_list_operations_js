56771b7abc56a727d040f52c4fb7bc499d8de407
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2024-08-10 19:35:24 -0500

    refactoring intersection

--- intersection.js
+++ intersection.js
@@ -4,16 +4,10 @@
  * @param {*} list2 
  * @returns 
  */
-function intersect(list1, list2) {
-    // Convertimos ambas listas a Set
-    let set1 = new Set(list1);
-    let set2 = new Set(list2);
-
-    // Unimos ambos sets
-    let intersection = new Set([...set1].filter(blog1 =>
+function intersect(set1, set2) {
+    return new Set([...set1].filter(blog1 =>
         [...set2].some(blog2 => blog1.url === blog2.url)
     ));
-    return intersection;
 }
 
 /**
@@ -21,23 +15,23 @@ function intersect(list1, list2) {
  */
 function main() {
 
-    let yesterdayBlogs = [
+    const yesterdayBlogs = new Set([
         { url: "https://www.joelonsoftware.com/", author: "Joel Spolsky" },
         { url: "https://blog.codinghorror.com/", author: "Jeff Atwood" },
         { url: "https://thedailywtf.com/", author: "Alex Papadimoulis" },
         { url: "https://alistapart.com/", author: "Jeffrey Zeldman" },
         { url: "https://overreacted.io/", author: "Dan Abramov" }
-    ];
+    ]);
 
-    let todayBlogs = [
+    const todayBlogs = new Set([
         { url: "https://waitbutwhy.com/", author: "Tim Urban" },
         { url: "https://thedailywtf.com/", author: "Alex Papadimoulis" }, // Duplicado
         { url: "https://solocodigoweb.com/", author: "Gonzalo Chacaltana" },
         { url: "https://overreacted.io/", author: "Dan Abramov" } // Duplicado
-    ];
+    ]);
 
-    // Realizamos la intersección de las dos listas de blogs
-    let commonBlogs = intersect(yesterdayBlogs, todayBlogs);
+    // Realizamos la intersección de los dos conjuntos de datos
+    const commonBlogs = intersect(yesterdayBlogs, todayBlogs);
 
     // Mostramos los blogs comunes
     console.log("Blogs comunes en ambas listas:");
